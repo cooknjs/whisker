6f41c368db6e6ba4d2f29801f31a7874390d846c
spadino <spada.andrea@gmail.com>
2021-08-23 14:55:53 -0300

    update eslint config and add mocha to test

--- test/commands/stir.test.js
+++ test/commands/stir.test.js
@@ -1,17 +1,18 @@
-const {expect, test} = require('@oclif/test')
+/* eslint-env mocha */
+const { expect, test } = require('@oclif/test')
 
 describe('stir', () => {
   test
-  .stdout()
-  .command(['stir'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+    .stdout()
+    .command(['stir'])
+    .it('runs hello', ctx => {
+      expect(ctx.stdout).to.contain('hello world')
+    })
 
   test
-  .stdout()
-  .command(['stir', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['stir', '--name', 'jeff'])
+    .it('runs hello --name jeff', ctx => {
+      expect(ctx.stdout).to.contain('hello jeff')
+    })
 })
